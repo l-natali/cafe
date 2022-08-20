2ae7f89d0b6fce32f22a6297fa49c5d52ee40f8c
l-natali <lukienko.natasha@gmail.com>
2022-08-20 19:37:02 +0300

    reservation form

--- base/views.py
+++ base/views.py
@@ -9,7 +9,7 @@ def base(request):
         reservation = UserReservationForm(request.POST)
         if reservation.is_valid():
             reservation.save()
-            return redirect('/', context={'reservation_form': reservation})
+            return redirect('/')
 
     categories = Category.objects.filter(is_visible=True)
     dishes = Dish.objects.filter(is_visible=True)
